0939c565c7b3b41d538dc22152c427701285efd9
Madalina-Maria Mitricioaei <madalina.mitricioaei@outlook.com>
2016-02-15 16:42:41 +0200

    Create CampText.cpp

---
+++ CampText.cpp
@@ -0,0 +1,13 @@
+#include "CampText.h"
+
+CampText::CampText(string titlu, int lungime, string id) // Constructor pentru camptext
+{
+    this->titlu = titlu;
+    this->lungime = lungime;
+    this->id = id;
+}
+
+CampText::~CampText()
+{
+    //dtor
+}
